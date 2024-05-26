1ec3a79313a4263e8003cd800e60d88b7258771c
earnpat <boonyanuch.patta@gmail.com>
2024-05-26 22:35:51 +0700

    new decker

--- grpc/grpc_client/main.go
+++ grpc/grpc_client/main.go
@@ -1,5 +1,3 @@
-// grpc-tutorial-topic-service/client/client.go
-// สำหรับการทดลองนี้ ให้แยกใส่โฟลเดอร์ client เพื่อไม่ให้มีปัญหาเรื่อง package
 package main
 
 import (
@@ -14,25 +12,7 @@ import (
 
 const (
 	address = "localhost:8080"
-	// defaultFilename = "topic.json"
 )
-
-// func parseFile(file string) (*pb.Topic, error) {
-// 	var topic *pb.Topic
-// 	// data, err := ioutil.ReadFile(file)
-// 	// if err != nil {
-// 	// 	return nil, err
-// 	// }
-// 	data := map[string]interface{}{
-// 		"title":       "Used computer for sale",
-// 		"description": "Lenevo ThinkPad T440",
-// 		"price":       300,
-// 		"category_id": "category001",
-// 	}
-// 	jsonStr, _ := json.Marshal(data)
-// 	json.Unmarshal(jsonStr, &topic)
-// 	return topic, nil
-// }
 
 func main() {
 	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
@@ -42,15 +22,6 @@ func main() {
 
 	defer conn.Close()
 	client := pb.NewTopicServiceClient(conn)
-	// file := defaultFilename
-	// if len(os.Args) > 1 {
-	// 	file = os.Args[1]
-	// }
-
-	// topic, err := parseFile(file)
-	// if err != nil {
-	// 	log.Fatalf("Could not parse file: %v", err)
-	// }
 
 	var topic *pb.Topic
 	data := map[string]interface{}{
@@ -62,26 +33,10 @@ func main() {
 	jsonStr, _ := json.Marshal(data)
 	json.Unmarshal(jsonStr, &topic)
 
-	// r, err := client.CreateTopic(context.Background(), topic)
-	// if err != nil {
-	// 	log.Fatalf("Could not greet: %v", err)
-	// }
-
-	// log.Printf("Created: %t", r.Created)
-	// getAll, err := client.GetTopics(context.Background(), &pb.GetRequest{})
-	// if err != nil {
-	// 	log.Fatalf("Could not list topics: %v", err)
-	// }
-
-	// for _, topic := range getAll.Topics {
-	// 	log.Println("topic: ", topic)
-	// }
-
-	hello, err := client.HelloTopic(context.Background(), &pb.GetRequest{})
+	response, err := client.HelloTopic(context.Background(), &pb.GetRequest{})
 	if err != nil {
 		log.Fatalf("Could not list topics: %v", err)
 	}
 
-	log.Println("hello: ", hello.Message)
-
+	log.Println("response: ", response.Message)
 }
--- grpc/main.go
+++ grpc/main.go
@@ -15,49 +15,11 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
-type Repository interface {
-	Create(*pb.Topic) (*pb.Topic, error)
-	GetAll() []*pb.Topic
-}
-type TopicRepository struct {
-	topics []*pb.Topic
-}
-
-func (repo *TopicRepository) Create(topic *pb.Topic) (*pb.Topic, error) {
-	updated := append(repo.topics, topic)
-	repo.topics = updated
-	return topic, nil
-}
-func (repo *TopicRepository) GetAll() []*pb.Topic {
-	return repo.topics
-}
-
 type service struct {
-	// repo Repository
 	pb.UnimplementedTopicServiceServer
 }
 
-// func NewHandler(repo Repository) pb.TopicServiceServer {
-// 	return &service{repo: repo}
-// }
-
-// func (s *service) CreateTopic(ctx context.Context, req *pb.Topic) (*pb.Response, error) {
-// 	topic, err := s.repo.Create(req)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	log.Println("Client call: CreateTopic")
-// 	return &pb.Response{Created: true, Topic: topic}, nil
-// }
-
-// func (s *service) GetTopics(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
-// 	topics := s.repo.GetAll()
-// 	log.Println("Client call: GetTopics")
-// 	return &pb.Response{Topics: topics}, nil
-// }
-
 func (s *service) HelloTopic(ctx context.Context, req *pb.GetRequest) (*pb.ResponseHello, error) {
-	// topics := s.repo.GetAll()
 	log.Println("Client call: HelloTopic")
 	timestamp := time.Now().Unix()
 	timeString := strconv.Itoa(int(timestamp))
@@ -67,15 +29,12 @@ func (s *service) HelloTopic(ctx context.Context, req *pb.GetRequest) (*pb.Respo
 
 func main() {
 	c := make(chan os.Signal, 1)
-	// repo := &TopicRepository{}
-	lis, err := net.Listen("tcp", "0.0.0.0:8080")
+	lis, err := net.Listen("tcp", ":9001")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	// grpcServer := grpc.NewServer(grpc.MaxConcurrentStreams(math.MaxUint32))
 	grpcServer := grpc.NewServer()
-	// grpcHandler := NewHandler(repo)
 	pb.RegisterTopicServiceServer(grpcServer, &service{})
 	reflection.Register(grpcServer)
 
