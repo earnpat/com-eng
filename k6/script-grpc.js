258cda0f3baa30ed58dfd832561963bd13070691
earnpat-performance-testing <boonyanuch.patta@gmaile.com>
2024-05-31 17:55:50 +0000

    update

--- k6/script-grpc.js
+++ k6/script-grpc.js
@@ -8,11 +8,11 @@ export const options = {
   vus: 10,
   // vus: 40,
   // vus: 70,
-  // vus: 100,
+  vus: 100,
   // duration: "1m",
   // duration: "5m",
-  duration: "15m",
-  // duration: "20m",
+  // duration: "15m",
+  duration: "20m",
   // duration: "30m",
 };
 
