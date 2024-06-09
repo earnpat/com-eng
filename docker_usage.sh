0ad4f0b8329541c51dd2b57414710d6f69d57632
earnpat <boonyanuch.patta@gmail.com>
2024-06-09 19:04:07 +0700

    script docker

--- docker_usage.sh
+++ docker_usage.sh
@@ -3,19 +3,22 @@
 CONTAINER_ID=8dd3a20adff9
 DURATION=5 # ระยะเวลาในการเก็บข้อมูล (s)
 INTERVAL=1 # ช่วงเวลาในการเก็บข้อมูล (s)
+OUTPUT_FILE="usage.csv"
 
 total_cpu=0
 total_memory=0
 count=0
 
+echo "time,cpu %,memory %" | tee -a $OUTPUT_FILE
+
 for ((i=1; i<DURATION; i+=INTERVAL)); do
     cpu=$(docker stats --no-stream --format "{{.CPUPerc}}" $CONTAINER_ID | tr -d '%')
     memory=$(docker stats --no-stream --format "{{.MemPerc}}" $CONTAINER_ID | tr -d '%')
 
-    echo "$(date '+%H:%M:%S')"
-    echo "CPU usage: $cpu%"
-    echo "Memory usage: $memory%"
-    echo ""
+    echo "$(date '+%Y-%m-%d %H:%M:%S'),$cpu,$memory" | tee -a $OUTPUT_FILE
+    # echo "CPU usage: $cpu%" | tee -a $OUTPUT_FILE
+    # echo "Memory usage: $memory%" | tee -a $OUTPUT_FILE
+    # echo ""
 
     total_cpu=$(echo "$total_cpu + $cpu" | bc)
     total_memory=$(echo "$total_memory + $memory" | bc)
@@ -24,9 +27,8 @@ for ((i=1; i<DURATION; i+=INTERVAL)); do
     # sleep $INTERVAL
 done
 
-average_cpu=$(echo "scale=2; $total_cpu / $count" | bc)
-average_memory=$(echo "scale=2; $total_memory / $count" | bc)
+# average_cpu=$(echo "scale=2; $total_cpu / $count" | bc)
+# average_memory=$(echo "scale=2; $total_memory / $count" | bc)
 
-echo "Average CPU usage: $average_cpu%"
-echo "Average Memory usage: $average_memory%"
-
+# echo "Average CPU usage: $average_cpu%" | tee -a $OUTPUT_FILE
+# echo "Average Memory usage: $average_memory%" | tee -a $OUTPUT_FILE
