6d5bffce9a102d6e1baa44444a6baa4b8d6070fd
earnpat <boonyanuch.patta@gmail.com>
2024-06-13 22:58:57 +0700

    delete comment

--- docker_usage.sh
+++ docker_usage.sh
@@ -16,19 +16,9 @@ for ((i=1; i<DURATION; i+=INTERVAL)); do
     memory=$(docker stats --no-stream --format "{{.MemPerc}}" $CONTAINER_ID | tr -d '%')
 
     echo "$(date '+%Y-%m-%d %H:%M:%S'),$cpu,$memory" | tee -a $OUTPUT_FILE
-    # echo "CPU usage: $cpu%" | tee -a $OUTPUT_FILE
-    # echo "Memory usage: $memory%" | tee -a $OUTPUT_FILE
-    # echo ""
 
     total_cpu=$(echo "$total_cpu + $cpu" | bc)
     total_memory=$(echo "$total_memory + $memory" | bc)
 
     count=$((count+1))
-    # sleep $INTERVAL
 done
-
-# average_cpu=$(echo "scale=2; $total_cpu / $count" | bc)
-# average_memory=$(echo "scale=2; $total_memory / $count" | bc)
-
-# echo "Average CPU usage: $average_cpu%" | tee -a $OUTPUT_FILE
-# echo "Average Memory usage: $average_memory%" | tee -a $OUTPUT_FILE
